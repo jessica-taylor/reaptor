c854ec403517e1586093a0fb5c0f664d4046a649
Jessica Taylor <jessi.liu.taylor@gmail.com>
2022-09-26 16:25:18 -0400

    mappings

--- src/language.rs
+++ src/language.rs
@@ -1,3 +1,13 @@
+use std::collections::BTreeMap;
+
+trait VarMapper<V1, V2> {
+    fn get_global_word(&self, word: &V1) -> Result<V2, String>;
+    fn get_global_ptr(&self, ptr: &V1) -> Result<V2, String>;
+    fn get_local_word(&self, word: &V1) -> Result<V2, String>;
+    fn get_local_ptr(&self, ptr: &V1) -> Result<V2, String>;
+    fn get_module(&self, module: &V1) -> Result<V2, String>;
+    fn get_procedure(&self, module: &V1, procedure: &V1) -> Result<V2, String>;
+}
 
 enum VMType {
     Word,
@@ -11,16 +21,70 @@ enum VMPtrType {
     Rc
 }
 
+struct Counts {
+    words: usize,
+    ptrs: usize
+}
+
+struct Locals<V> {
+    words: Vec<V>,
+    ptrs: Vec<V>,
+}
+
+impl<V> Locals<V> {
+    fn count(&self) -> Counts {
+        Counts {
+            words: self.words.len(),
+            ptrs: self.ptrs.len(),
+        }
+    }
+
+    fn map<V2>(&self, mapper: &dyn VarMapper<V, V2>) -> Result<Locals<V2>, String> {
+        let mut words = Vec::new();
+        let mut ptrs = Vec::new();
+        for word in &self.words {
+            words.push(mapper.get_local_word(word)?);
+        }
+        for ptr in &self.ptrs {
+            ptrs.push(mapper.get_local_ptr(ptr)?);
+        }
+        Ok(Locals {
+            words: words,
+            ptrs: ptrs,
+        })
+    }
+}
+
 enum VMWordLValue<V> {
     Local(V),
     Global(V),
     Index(Box<VMPtrLValue<V>>, Box<VMWordRValue<V>>),
 }
 
+impl<V> VMWordLValue<V> {
+    fn map<V2>(&self, mapper: &dyn VarMapper<V, V2>) -> Result<VMWordLValue<V2>, String> {
+        Ok(match self {
+            VMWordLValue::Local(v) => VMWordLValue::Local(mapper.get_local_word(v)?),
+            VMWordLValue::Global(v) => VMWordLValue::Global(mapper.get_global_word(v)?),
+            VMWordLValue::Index(ptr, index) => VMWordLValue::Index(Box::new(ptr.map(mapper)?), Box::new(index.map(mapper)?)),
+        })
+    }
+}
+
 enum VMPtrLValue<V> {
     Local(V),
     Global(V),
     Index(Box<VMPtrLValue<V>>, Box<VMWordRValue<V>>),
+}
+
+impl<V> VMPtrLValue<V> {
+    fn map<V2>(&self, mapper: &dyn VarMapper<V, V2>) -> Result<VMPtrLValue<V2>, String> {
+        Ok(match self {
+            VMPtrLValue::Local(v) => VMPtrLValue::Local(mapper.get_local_ptr(v)?),
+            VMPtrLValue::Global(v) => VMPtrLValue::Global(mapper.get_global_ptr(v)?),
+            VMPtrLValue::Index(ptr, index) => VMPtrLValue::Index(Box::new(ptr.map(mapper)?), Box::new(index.map(mapper)?)),
+        })
+    }
 }
 
 enum VMWordRValue<V> {
@@ -32,20 +96,51 @@ enum VMWordRValue<V> {
     PtrLengthPtr(Box<VMPtrLValue<V>>),
 }
 
+impl<V> VMWordRValue<V> {
+    fn map<V2>(&self, mapper: &dyn VarMapper<V, V2>) -> Result<VMWordRValue<V2>, String> {
+        Ok(match self {
+            VMWordRValue::Local(v) => VMWordRValue::Local(mapper.get_local_word(v)?),
+            VMWordRValue::Global(v) => VMWordRValue::Global(mapper.get_global_word(v)?),
+            VMWordRValue::Index(ptr, index) => VMWordRValue::Index(Box::new(ptr.map(mapper)?), Box::new(index.map(mapper)?)),
+            VMWordRValue::PtrTag(ptr) => VMWordRValue::PtrTag(Box::new(ptr.map(mapper)?)),
+            VMWordRValue::PtrLengthWord(ptr) => VMWordRValue::PtrLengthWord(Box::new(ptr.map(mapper)?)),
+            VMWordRValue::PtrLengthPtr(ptr) => VMWordRValue::PtrLengthPtr(Box::new(ptr.map(mapper)?)),
+        })
+    }
+}
+
 enum VMPtrRValue<V> {
-    Copy(VMPtrLValue<V>),
-    CloneRc(VMPtrLValue<V>),
+    Copy(Box<VMPtrLValue<V>>),
+    CloneRc(Box<VMPtrLValue<V>>),
+    Closure(V, V, Locals<Box<VMPtrRValue<V>>>), // module, function, curries
 }
 
-struct Counts {
-    words: usize,
-    ptrs: usize
+impl<V> VMPtrRValue<V> {
+    fn map<V2>(&self, mapper: &dyn VarMapper<V, V2>) -> Result<VMPtrRValue<V2>, String> {
+        Ok(match self {
+            VMPtrRValue::Copy(ptr) => VMPtrRValue::Copy(Box::new(ptr.map(mapper)?)),
+            VMPtrRValue::CloneRc(ptr) => VMPtrRValue::CloneRc(Box::new(ptr.map(mapper)?)),
+            VMPtrRValue::Closure(module, function, curries) => {
+                let mut cur_words = Vec::new();
+                let mut cur_ptrs = Vec::new();
+                for cur in &curries.words {
+                    cur_words.push(Box::new(cur.map(mapper)?));
+                }
+                for cur in &curries.ptrs {
+                    cur_ptrs.push(Box::new(cur.map(mapper)?));
+                }
+                VMPtrRValue::Closure(
+                    mapper.get_module(module)?,
+                    mapper.get_procedure(module, function)?,
+                    Locals {
+                        words: cur_words,
+                        ptrs: cur_ptrs,
+                    })
+            }
+        })
+    }
 }
 
-struct Locals<V> {
-    words: Vec<V>,
-    ptrs: Vec<V>,
-}
 
 enum VMStatement<V> {
     SetWord(VMWordLValue<V>, VMWordRValue<V>),
@@ -54,14 +149,13 @@ enum VMStatement<V> {
     SwapPtr(VMWordLValue<V>, VMPtrLValue<V>),
     Alloc(VMPtrLValue<V>, VMWordRValue<V>),
     Call(V, V, Locals<V>, Locals<V>), // module, function, args, returns
-    Closure(V, V, Locals<V>), // module, function, curries
     CallPtr(VMPtrLValue<V>, Locals<V>, Locals<V>),
     Return(Locals<V>)
 }
 
 struct VMProcedure<V> {
-    param_counts: Counts,
-    local_counts: Counts, // must be >= param_counts
+    params: Locals<V>,
+    local_counts: Counts,
     return_counts: Counts, // must be <= local_counts
     statements: Vec<VMStatement<V>>,
 }
@@ -69,4 +163,72 @@ struct VMProcedure<V> {
 struct VMModule<V> {
     globals: Counts,
     procedures: Vec<VMProcedure<V>>,
+}
+
+struct VarProcedureManager {
+    local_words: BTreeMap<String, usize>,
+    local_ptrs: BTreeMap<String, usize>,
+    params: Locals<usize>,
+}
+
+struct VarModuleManager {
+    global_words: BTreeMap<String, usize>,
+    global_ptrs: BTreeMap<String, usize>,
+    procedures: BTreeMap<String, usize>,
+    procedure_managers: Vec<VarProcedureManager>,
+}
+
+struct VarLibraryManager {
+    modules: BTreeMap<String, usize>,
+    module_managers: Vec<VarModuleManager>,
+}
+
+fn add_index_to_map(map: &mut BTreeMap<String, usize>, name: &str) -> usize {
+    let index = map.len();
+    if map.insert(name.to_string(), index).is_some() {
+        panic!("duplicate variable name");
+    }
+    index
+}
+
+impl VarProcedureManager {
+    fn new() -> Self {
+        VarProcedureManager {
+            local_words: BTreeMap::new(),
+            local_ptrs: BTreeMap::new(),
+            params: Locals {
+                words: Vec::new(),
+                ptrs: Vec::new(),
+            },
+        }
+    }
+    fn add_local_word(&mut self, name: &str) -> usize {
+        add_index_to_map(&mut self.local_words, name)
+    }
+    fn add_local_ptr(&mut self, name: &str) -> usize {
+        add_index_to_map(&mut self.local_ptrs, name)
+    }
+    fn set_params(&mut self, params: Locals<String>) {
+
+    }
+}
+
+impl VarModuleManager {
+    fn new() -> Self {
+        VarModuleManager {
+            global_words: BTreeMap::new(),
+            global_ptrs: BTreeMap::new(),
+            procedures: BTreeMap::new(),
+            procedure_managers: Vec::new(),
+        }
+    }
+    fn add_global_word(&mut self, name: String) -> usize {
+        add_index_to_map(&mut self.global_words, &name)
+    }
+    fn add_global_ptr(&mut self, name: String) -> usize {
+        add_index_to_map(&mut self.global_ptrs, &name)
+    }
+    fn add_procedure(&mut self, name: String) -> usize {
+        add_index_to_map(&mut self.procedures, &name)
+    }
 }
